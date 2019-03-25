32a654751e5fdb82b2f1c88fd58d43bc1dbe3428
Scott Dowding <scottddowding1279@gmail.com>
2019-03-25 15:48:11 -0400

    add daysInMonth function

--- index.js
+++ index.js
@@ -50,3 +50,40 @@ function decode(code) {
 
 // console.log(decode('craft block argon meter bells brown croon droop'));
 
+function daysInMonth(month, leapYear = false) {
+  month = month.toLowerCase();
+  let result = '';
+  switch(month) {
+  case 'january':
+  case 'march':
+  case 'may':
+  case 'july':
+  case 'august':
+  case 'october':
+  case 'december':
+    result = `${month} has 31 days`;
+    break;
+  case 'february':
+    if (leapYear) {
+      result = `${month} has 29 days`;
+    } else {
+      result = `${month} has 28 days`;
+    }
+    break;
+  case 'april':
+  case 'june':
+  case 'september':
+  case 'november':
+    result = `${month} has 30 days`;
+    break;
+  default:
+    throw new Error('Must provide a valid month');
+  }
+  return result;
+}
+
+try {
+  console.log(daysInMonth('fatbruary', true));
+} catch(e) {
+  console.log(e.message);
+}
