049d19fc739254846f9e201e66f1cebcd87bd108
Scott Dowding <scottddowding1279@gmail.com>
2019-03-25 15:14:41 -0400

    added beyond function

--- index.js
+++ index.js
@@ -4,4 +4,22 @@ function jediName(firstName, lastName) {
   return lastName.slice(0, 3) + firstName.slice(0, 2);
 }
 
-console.log(jediName('Scott', 'Dowding'));
+// console.log(jediName('Scott', 'Dowding'));
+
+function beyond(num) {
+  if (num === Infinity || num === -Infinity) {
+    console.log('And beyond');
+  } else if (num > 0) {
+    console.log('To infinity');
+  } else if (num < 0) {
+    console.log('To negative infinity');
+  } else {
+    console.log('Staying home');
+  }
+}
+
+// beyond(Infinity);
+// beyond(-Infinity);
+// beyond(-1);
+// beyond(1);
+// beyond(0);
